16499c96d55141b768a5ebe9a0667ed02310a1f5
LakiG <garg.lakshya@gmail.com>
2022-07-22 14:08:23 +0530

    TestRunner reorganized

--- test_runner.py
+++ test_runner.py
@@ -13,6 +13,15 @@ class TestRunner():
         self.job_data = None
         self.auxiliary_space = {}
 
+    def validate_logic(self, endpoint_model, json_data, message):
+
+        try:
+            ENDPOINT_TO_MODEL[endpoint_model](**json_data)
+            print(f'{message} Schema validation |successful| for {self.job_data["operation"]} {self.job_data["endpoint"]}')
+        except ValidationError as err:
+            print(f'{message} Schema validation |failed| for {self.job_data["operation"]} {self.job_data["endpoint"]}')
+            print(err)
+
     def validate_request_body(self, request_body):
 
         request_body_json = None
@@ -24,13 +33,9 @@ class TestRunner():
             print(f"Error in request body - {err}")
 
         # Logical Schema Validation
-        try:
-            endpoint_model = self.job_data["name"] + "_request_body"
 
-            ENDPOINT_TO_MODEL[endpoint_model](**request_body_json)
-            print(f'Request Body Schema validation |successful| for {self.job_data["endpoint"]}')
-        except ValidationError as err:
-            print(err)
+        endpoint_model = self.job_data["name"] + "_request_body"
+        self.validate_logic(endpoint_model, request_body_json, "Request Body")
 
     def validate_response(self, response):
 
@@ -42,27 +47,19 @@ class TestRunner():
             print("Request Failed. Incorrect Response Status Code.")
 
         # Logical Schema Validation
-
         if not response.text:
             response_json = {}
         else:
             response_json = response.json()
+        print(response_json)
 
-        try:
-            endpoint_model = None
-            if self.job_data["name"] in ["list_tasks", "get_task"]:
-                view_query = [item["view"] for item in self.job_data["query_parameters"]]
-                endpoint_model = self.job_data["name"] + "_" + view_query[0]
-            else:
-                endpoint_model = self.job_data["name"]
-
-            ENDPOINT_TO_MODEL[endpoint_model](**response_json)
-            print(f'Response Schema validation |successful| for {self.job_data["operation"]} {self.job_data["endpoint"]}')
-
-            self.auxiliary_space[self.job_data["name"]] = response_json
-            # print(self.auxiliary_space)
-        except ValidationError as err:
-            print(err)
+        if self.job_data["name"] in ["list_tasks", "get_task"]:
+            view_query = [item["view"] for item in self.job_data["query_parameters"]]
+            endpoint_model = self.job_data["name"] + "_" + view_query[0]
+        else:
+            endpoint_model = self.job_data["name"]
+        self.validate_logic(endpoint_model, response_json, "Response")
+        self.auxiliary_space[self.job_data["name"]] = response_json
 
     def run_tests(self, job_data):
 
@@ -72,11 +69,9 @@ class TestRunner():
         id_uri_param = None
         query_params = {}
         request_body = None
-        response = None
 
-        if job_data["name"] in ["get_task"] and "polling" not in job_data.keys():
-            id_uri_param = self.auxiliary_space["list_tasks"]["tasks"][0]["id"]
-            # print(id_uri_param)
+        if job_data["name"] in ["get_task", "cancel_task"]:
+            id_uri_param = self.auxiliary_space["create_task"]["id"]
 
         if job_data["name"] in ["get_task", "list_tasks"]:
             for param in job_data["query_parameters"]:
@@ -88,13 +83,7 @@ class TestRunner():
             self.validate_request_body(request_body)
             # print(request_body)
 
-        if job_data["name"] in ["cancel_task"]:
-            id_uri_param = self.auxiliary_space["create_task"]["id"]
-            # print(id_uri_param)
-
         if "polling" in job_data.keys():
-
-            id_uri_param = self.auxiliary_space["create_task"]["id"]
 
             check_cancel = False
             # print(self.auxiliary_space)
