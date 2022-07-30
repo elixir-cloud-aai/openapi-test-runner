308124959a4ee07da63b714b1de6347bb93b14d9
LakiG <garg.lakshya@gmail.com>
2022-07-30 14:17:16 +0530

    Added cancel task functional test and minor summary update

--- job_runner.py
+++ job_runner.py
@@ -17,17 +17,27 @@ class JobRunner():
             "failed": 0,
             "skipped": 0
         }
+        self.test_status = {
+            "passed": [],
+            "failed": [],
+            "skipped": []
+        }
 
     def generate_summary(self):
+
+        passed_tests = ", ".join(self.test_status["passed"])
+        failed_tests = ", ".join(self.test_status["failed"])
+        skipped_tests = ", ".join(self.test_status["skipped"])
+
         print("\n\n\n")
-        print_yellow("{:#^50}".format("   Compliance Testing Summary   "))
-        print_yellow("#{:^48}#".format(""))
-        print_yellow("#{:^48}#".format(f"Total Tests - {self.test_count}"))
-        print_yellow("#{:^48}#".format(f'Passed - {self.result["passed"]}'))
-        print_yellow("#{:^48}#".format(f'Failed - {self.result["failed"]}'))
-        print_yellow("#{:^48}#".format(f'Skipped - {self.result["skipped"]}'))
-        print_yellow("#{:^48}#".format(""))
-        print_yellow("{:#^50}".format(""))
+        print_yellow("{:#^90}".format("   Compliance Testing Summary   "))
+        print_yellow("#{:^88}#".format(""))
+        print_yellow("#{:^88}#".format(f"Total Tests - {self.test_count}"))
+        print_yellow("#{:^88}#".format(f'Passed - {self.result["passed"]} ({passed_tests})'))
+        print_yellow("#{:^88}#".format(f'Failed - {self.result["failed"]} ({failed_tests})'))
+        print_yellow("#{:^88}#".format(f'Skipped - {self.result["skipped"]} ({skipped_tests})'))
+        print_yellow("#{:^88}#".format(""))
+        print_yellow("{:#^90}".format(""))
 
     def validate_job(self, yaml_data, yaml_file):
         schema_path = os.path.join(self.path, "tests", "template", "test_template_schema.json")
@@ -61,20 +71,23 @@ class JobRunner():
                         for tag in self.tags:
                             if tag in yaml_data["tags"]:
                                 tag_matched = True
-                                test_runner = TestRunner(yaml_data["server"], yaml_data["version"][0], self.result)
+                                test_runner = TestRunner(yaml_data["server"], yaml_data["version"][0])
                                 job_count = 0
                                 for job in yaml_data["jobs"]:
                                     job_count += 1
                                     print(f'Running tests for sub-job-{job_count} -> {job["name"]}')
                                     test_runner.run_tests(job)
                                 self.result["passed"] += 1
+                                self.test_status["passed"].append(str(self.test_count))
                                 print_green(f'Compliance Test-{self.test_count} for {yaml_file} successful.')
                         if not tag_matched:
                             self.result["skipped"] += 1
+                            self.test_status["skipped"].append(str(self.test_count))
                             print_blue(f"No Tag matched. Skipping Test-{self.test_count} for {yaml_file}")
 
                     except ComplianceException as err:
                         self.result["failed"] += 1
+                        self.test_status["failed"].append(str(self.test_count))
                         print_red(f'Compliance Test-{self.test_count} for {yaml_file} failed.')
                         print_red(f'Error - {err}')
 
