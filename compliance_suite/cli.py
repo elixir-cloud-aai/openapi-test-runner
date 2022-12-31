4f831fa26c0cdaf84fcacb1084a3d292d6019459
LakiG <garg.lakshya@gmail.com>
2022-12-31 21:29:08 +0530

    Add server and version in CLI args

--- compliance_suite/cli.py
+++ compliance_suite/cli.py
@@ -19,12 +19,16 @@ def main() -> None:
 
 
 @main.command(help='Run TES compliance tests against the servers')
+@click.option('--server', '-s', help='server URL on which the compliance tests are run. Format - https://<url>/')
+@click.option('--version', '-v', help='TES version. Example - "v1.0"')
 @click.option('--tag', '-t', multiple=True, help='Tag', default=['All'])
 @click.option('--output_path', '-o', help='path to output the JSON report')
 @click.option('--serve', default=False, is_flag=True, help='spin up a server')
 @click.option('--port', default=15800, help='port at which the compliance report is served')
 @click.option('--uptime', '-u', default=3600, help='time that server will remain up in seconds')
-def report(tag: List[str],
+def report(server: str,
+           version: str,
+           tag: List[str],
            output_path: str,
            serve: bool,
            port: int,
@@ -33,6 +37,9 @@ def report(tag: List[str],
     Run the compliance suite for the given tags.
 
     Args:
+        server (str): The server URL on which the compliance suite will be run. Format - https://<url>/
+        version (str): The compliance suite will be run against this TES version.
+                       Default - Latest version. Example - "v1.0"
         tag (List[str]): The list of the tags for which the compliance suite will be run. Default - All
         output_path (str): The output path to store the JSON compliance report
         serve (bool): If true, runs a local server and displays the JSON report in webview
@@ -40,9 +47,16 @@ def report(tag: List[str],
         uptime (int): The local server duration in seconds. Default - 3600 seconds
     """
 
+    if server is None:
+        raise Exception("No server provided. Please provide a server URL.")
+
+    if version is None:
+        logger.info("No version provided. Latest version is used as default value.")
+        version = "v1.0"                    # Hardcode the latest version here. TODO - Future versions
+
     tag = [val.lower() for val in tag]      # Convert the tags into lowercase to allow case-insensitive tags
     logger.info(f"Input tag is - {tag}")
-    job_runner = JobRunner(tag)
+    job_runner = JobRunner(server, version, tag)
     job_runner.run_jobs()
 
     json_report = job_runner.generate_report()
