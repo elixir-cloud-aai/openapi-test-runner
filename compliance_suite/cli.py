8f990775d7f5ec463c2026dbaf77ed5be876dfdb
Lakshya Garg <garg.lakshya@gmail.com>
2023-07-17 20:09:53 +0530

    feat: add support for test-path (#56)

--- compliance_suite/cli.py
+++ compliance_suite/cli.py
@@ -4,6 +4,7 @@ This module is the entry point for the compliance suite and contains a CLI funct
 """
 
 import os
+from pathlib import Path
 import re
 from typing import (
     Any,
@@ -51,6 +52,8 @@ def validate_regex(ctx: Any, param: Any, value: List[str]):
               help='run tests for provided tags', callback=validate_regex)
 @click.option('--exclude-tags', '-e', 'exclude_tags', multiple=True,
               help='skip tests for provided tags', callback=validate_regex)
+@click.option('--test-path', '-tp', 'test_path', multiple=True,
+              help='the absolute or relative path of the tests to be run', default=["tests"])
 @click.option('--output_path', '-o', help='path to output the JSON report')
 @click.option('--serve', default=False, is_flag=True, help='spin up a server')
 @click.option('--port', default=15800, help='port at which the compliance report is served')
@@ -59,6 +62,7 @@ def report(server: str,
            version: str,
            include_tags: List[str],
            exclude_tags: List[str],
+           test_path: List[str],
            output_path: str,
            serve: bool,
            port: int,
@@ -71,11 +75,17 @@ def report(server: str,
         version (str): The compliance suite will be run against this TES version. Example - "1.0.0"
         include_tags (List[str]): The list of the tags for which the compliance suite will be run.
         exclude_tags (List[str]): The list of the tags for which the compliance suite will not be run.
+        test_path: The list of absolute or relative paths from the project root of the test file/directory.
+            Default - ["tests"]
         output_path (str): The output path to store the JSON compliance report
         serve (bool): If true, runs a local server and displays the JSON report in webview
         port (int): Set the local server port. Default - 16800
         uptime (int): The local server duration in seconds. Default - 3600 seconds
     """
+
+    for path in test_path:
+        if not Path(path).exists():
+            raise FileNotFoundError(f"Test path: {path} not found. Please provide a valid path.")
 
     # Convert the tags into lowercase to allow case-insensitive tags
     include_tags = [val.lower() for val in include_tags]
@@ -83,8 +93,10 @@ def report(server: str,
 
     logger.info(f"Provided server: {server} version: {version}")
     logger.info(f"Provided tags - include: {include_tags} exclude: {exclude_tags}")
+    logger.info(f"Provided test path: {test_path}")
     job_runner = JobRunner(server, version)
     job_runner.set_tags(include_tags, exclude_tags)
+    job_runner.set_test_path(test_path)
     job_runner.run_jobs()
 
     json_report = job_runner.generate_report()
--- unittests/test_job_runner.py
+++ unittests/test_job_runner.py
@@ -3,7 +3,7 @@
 This module is to test the Job Runner class and its methods
 """
 
-import os
+from pathlib import Path
 from unittest.mock import (
     MagicMock,
     patch
@@ -16,18 +16,17 @@ from compliance_suite.exceptions.compliance_exception import (
     JobValidationException,
     TestRunnerException
 )
+from compliance_suite.functions.report import Report
 from compliance_suite.job_runner import JobRunner
 from compliance_suite.test_runner import TestRunner
 from unittests.data.constants import TEST_URL
 
 
-SCHEMA_PATH = os.path.join(os.getcwd(), "tests", "template", "test_template_schema.json")
-YAML_TEST_PATH = os.path.join(os.getcwd(), "unittests", "data", "run_job_tests")
-YAML_TEST_PATH_SUCCESS = os.path.join(os.getcwd(), "unittests", "data", "run_job_tests", "success_01.yml")
-YAML_TEST_PATH_INVALID = os.path.join(os.getcwd(), "unittests", "data", "run_job_tests", "invalid_yaml.yml")
-YAML_TEST_PATH_SKIP = os.path.join(os.getcwd(), "unittests", "data", "run_job_tests", "skip_01.yml")
-YAML_TEST_PATH_FAIL = os.path.join(os.getcwd(), "unittests", "data", "run_job_tests", "fail_service_info.yml")
-YAML_WRONG_SCHEMA = os.path.join(os.getcwd(), "unittests", "data", "tests", "wrong_schema_yaml.yml")
+YAML_TEST_PATH_SUCCESS = Path("unittests/data/run_job_tests/success_01.yml")
+YAML_TEST_PATH_INVALID = Path("unittests/data/run_job_tests/invalid_yaml.yml")
+YAML_TEST_PATH_SKIP = Path("unittests/data/run_job_tests/skip_01.yml")
+YAML_TEST_PATH_FAIL = Path("unittests/data/run_job_tests/fail_service_info.yml")
+YAML_WRONG_SCHEMA = Path("unittests/data/tests/wrong_schema_yaml.yml")
 
 
 class TestJobRunner:
@@ -46,8 +45,7 @@ class TestJobRunner:
         job_runner_object.generate_report()
         assert True
 
-    @patch("os.path.join", return_value=SCHEMA_PATH)
-    def test_validate_job_success(self, mock_os):
+    def test_validate_job_success(self):
         """ Asserts validate job functions for proper YAML schema"""
 
         with open(YAML_TEST_PATH_SUCCESS, "r") as f:
@@ -56,8 +54,7 @@ class TestJobRunner:
         job_runner_object = JobRunner(TEST_URL, "1.0.0")
         assert job_runner_object.validate_job(yaml_data, "success_01.yml") is None
 
-    @patch('os.path.join', return_value=SCHEMA_PATH)
-    def test_validate_job_failure(self, mock_os):
+    def test_validate_job_failure(self):
         """ Asserts validate_job() function for incorrect YAML schema"""
 
         with open(YAML_WRONG_SCHEMA, "r") as f:
@@ -67,17 +64,39 @@ class TestJobRunner:
             job_runner_object = JobRunner(TEST_URL, "1.0.0")
             job_runner_object.validate_job(yaml_data, "wrong_schema_yaml.yml")
 
+    @patch.object(JobRunner, 'initialize_test')
+    def test_run_jobs_file(self, mock_initialize_test):
+        job_runner_object = JobRunner(TEST_URL, "1.0.0")
+        job_runner_object.set_test_path(["unittests/data/run_job_tests/success_01.yml"])
+        job_runner_object.run_jobs()
+        mock_initialize_test.assert_called_once()
+
+    @patch.object(JobRunner, 'initialize_test')
+    def test_run_jobs_dir(self, mock_initialize_test):
+        job_runner_object = JobRunner(TEST_URL, "1.0.0")
+        job_runner_object.set_test_path(["unittests/data/run_job_tests"])
+        job_runner_object.run_jobs()
+        assert mock_initialize_test.call_count == len(list(Path("unittests/data/run_job_tests").glob("**/*.yml")))
+
     @patch.object(JobRunner, 'validate_job')
     @patch.object(TestRunner, 'run_tests')
-    @patch('os.path.join')
-    def test_run_jobs_success(self, mock_os, mock_run_tests, mock_validate_job):
-        """ Asserts run_jobs() for unit test YAML files"""
-
-        mock_run_tests.side_effect = [TestRunnerException(name="test", message="test", details="test"), None, None]
-        mock_validate_job.return_value = {}
-
-        mock_os.side_effect = [YAML_TEST_PATH, YAML_TEST_PATH_FAIL, YAML_TEST_PATH_INVALID, YAML_TEST_PATH_SKIP,
-                               YAML_TEST_PATH_SUCCESS]
+    def test_initialize_test(self, mock_run_tests, mock_validate_job):
         job_runner_object = JobRunner(TEST_URL, "1.0.0")
-        job_runner_object.set_tags(["schema_validation_only"], [])
-        assert job_runner_object.run_jobs() is None
+        job_runner_object.set_report(Report())
+
+        job_runner_object.initialize_test(YAML_TEST_PATH_SUCCESS)
+        assert len(job_runner_object.test_status["passed"]) == 1
+
+        job_runner_object.initialize_test(YAML_TEST_PATH_SKIP)
+        assert len(job_runner_object.test_status["skipped"]) == 1
+
+        mock_run_tests.side_effect = [TestRunnerException("test", "test", "test")]
+        job_runner_object.initialize_test(YAML_TEST_PATH_FAIL)
+        assert len(job_runner_object.test_status["failed"]) == 1
+
+    @patch.object(TestRunner, 'run_tests')
+    def test_initialize_test_invalid_job(self, mock_run_tests):
+        job_runner_object = JobRunner(TEST_URL, "1.0.0")
+        job_runner_object.set_report(MagicMock())
+        job_runner_object.initialize_test(YAML_TEST_PATH_INVALID)
+        assert len(job_runner_object.test_status["failed"]) == 1
